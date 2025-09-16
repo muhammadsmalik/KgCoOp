b9e018a2dc88285d50303033a3028c58cfad303b
Salman Malik <salmalik64@gmail.com>
2025-09-16 19:50:09 +0800

    Add OfficeHomeDG prompt template

--- KgCoOp/trainers/kgcoop.py
+++ KgCoOp/trainers/kgcoop.py
@@ -51,6 +51,7 @@ CUSTOM_TEMPLATES = {
     "ImageNetV2": "a photo of a {}.",
     "ImageNetA": "a photo of a {}.",
     "ImageNetR": "a photo of a {}.",
+    "OfficeHomeDG": "a photo of a {}.",
 }
 
 
